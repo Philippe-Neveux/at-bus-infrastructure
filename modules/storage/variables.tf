607271035960096f81fa833ef41e61673afadb30
Philippe Neveux <phil.neveux@gmail.com>
2025-07-10 10:52:59 +1200

    rename bucket

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -6,5 +6,5 @@ variable "location" {
 variable "pne_open_data_bucket_name" {
   description = "The name of the PNE open data bucket"
   type        = string
-  default     = "pne-open-data"
+  default     = "at-bus-open-data"
 }
